73d4b1fb4ac18a7991323161dbf23a9afba1f2a4
jesus mclouis <mclouisre@gmail.com>
2023-06-18 22:41:49 +0200

    Error checking

--- config/app.config.js
+++ config/app.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config;
 
 const MONGO_DB_CONFIG = {
-    DB: 'mongodb+srv://mclouisre:ultimatestars7@cluster0.e4je1lo.mongodb.net/'
+    DB: 'mongodb+srv://mclouisre:@ultimatestars7.e4je1lo.mongodb.net/?retryWrites=true&w=majority'
 }
 
 module.exports = {
